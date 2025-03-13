b0e7d4ff6b091eef30ba3cd7301b7a073ea02134
Edwin Burwell <edwinburwell758@gmail.com>
2025-03-12 21:11:15 -0500

    Add new project.

---
+++ vk_test_v2/main.cpp
@@ -0,0 +1,51 @@
+#include <vulkan/vulkan.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <cstdlib>
+#include <iostream>
+
+class App {
+  GLFWwindow* window;
+
+public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  void initWindow();
+
+  void initVulkan() {}
+
+  void mainLoop() {}
+
+  void cleanup() {}
+};
+
+int main() {
+  App app;
+
+  try {
+    app.run();
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
+
+void App::initWindow() {
+  glfwInit();
+
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+  window =
+      glfwCreateWindow(500, 500, "Window Title Placeholder", nullptr, nullptr);
+}
---
+++ vk_test_v2/util.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vulkan/vulkan_raii.hpp>
+
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_video.h>
+
+// Probably the most baller `todo!()` implementation there ever was.
+struct TODO {
+    [[noreturn]] TODO() {
+        throw std::logic_error("TODO: Not implemented!");
+    }
+
+    template<typename T>
+    [[noreturn]] operator T() const {
+        throw std::logic_error("TODO: This should be unreachable.");
+    }
+};
+
+template<typename T>
+inline T expect(std::optional<T> wrapped, char const* msg) {
+    if (!wrapped) {
+        throw std::runtime_error(msg);
+    }
+
+    return wrapped.value();
+}
